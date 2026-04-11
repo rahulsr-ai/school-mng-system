d32bd4f7e2cea9e4dccdf8971e190ba1ed05118d
rahulsr-ai <rahulsinghrawat085@gmail.com>
2026-04-11 16:23:45 +0530

    cors url updated

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://gridedu.netlify.app/"], 
+        origin: ["https://gridedu.netlify.app"], 
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     }
